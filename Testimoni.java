1ea21ff5025d14bc0b4ebfd031731bfe74469c37
fazuuka <168648127+fazuuka@users.noreply.github.com>
2024-06-02 01:45:18 +0800

    Add files via upload

---
+++ Testimoni.java
@@ -0,0 +1,103 @@
+package com.gotpb.tubespbokelompok7.model;
+
+
+import java.lang.reflect.Array;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.gotpb.tubespbokelompok7.DatabaseConnection;
+import com.gotpb.tubespbokelompok7.Json;
+
+
+public class Testimoni {
+    public int id;
+    public User user;
+    public String text;
+
+    public Testimoni(String text) {
+//        this.id = id;
+//        this.idUser = idUser;
+        this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//
+//    public String getIdUser() {
+//        return idUser;
+//    }
+//
+//    public void setIdUser(String idUser) {
+//        this.idUser = idUser;
+//    }
+
+
+    public static ArrayList<Testimoni> getAll() {
+        ArrayList<Testimoni> listTestimoni = new ArrayList<>();
+        String query = "SELECT * FROM table_testimoni";
+        DatabaseConnection db = DatabaseConnection.getInstance();
+
+        try {
+            PreparedStatement statement = db.connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                Testimoni testimoni = new Testimoni(
+//                        resultSet.getString("id"),
+//                        resultSet.getString("id_user"),
+                        resultSet.getString("text")
+                );
+
+                listTestimoni.add(testimoni);
+            }
+            return listTestimoni;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void addTestimoni(String idUser, String text) {
+        String query = "INSERT INTO table_testimoni (id_user, text) VALUES (?,?)";
+        DatabaseConnection db = DatabaseConnection.getInstance();
+
+        try {
+            PreparedStatement statement = db.connection.prepareStatement(query);
+            statement.setString(1, idUser);
+            statement.setString(2, text);
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
